6bd57a065f25f5b06091080f46fd67683d5dfa73
VChekS <starow1991@gmail.com>
2022-09-27 16:19:30 +0500

    1

---
+++ src/main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+
+
+        int x = 5_030;
+        int mile = x / 20;
+        int spent = x;
+
+        System.out.println("ВСего бонусных миль зачислино: " + mile);
+
+
+
+    }
+
+
+}
