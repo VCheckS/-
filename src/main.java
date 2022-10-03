f520a7c9f9a63fee2d89d9467f33f180eaf147d8
VChekS <starow1991@gmail.com>
2022-10-03 13:09:41 +0500

    corretcing 2

--- src/main.java
+++ src/main.java
@@ -2,12 +2,11 @@ public class main {
     public static void main(String[] args) {
 
 
-        int x = 5_030;
-        int mile = x / 20;
-        int spent = x;
+        int spent = 5_030;
+        int mile = spent / 20;
+
 
         System.out.println("ВСего бонусных миль зачислино: " + mile);
-
 
 
     }
